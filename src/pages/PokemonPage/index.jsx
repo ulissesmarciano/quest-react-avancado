24cf9c828362a509aaaef304de0df1056602b58b
Ulisses <ulissesmarcianodev@gmail.com>
2025-05-27 21:30:56 -0300

    feat: adiciona itens chamados na api e estiliza

--- src/pages/PokemonPage/index.jsx
+++ src/pages/PokemonPage/index.jsx
@@ -1,11 +1,17 @@
 import { Link, useParams } from "react-router-dom"
 import useFetchPokemonData from "../../hooks/useFetchPokemon"
 import InfoTabScreen from "../../components/info-tab-screen"
+import { pokemonTypeColors } from "../../constants/colors"
 import styled from "styled-components"
 
 const Container = styled.main`
+    height: calc(100vh - 84.97px);
+
     padding: 1rem 2rem;
-    background-color: aquamarine;
+
+    @media(min-width: 640px){
+        height: calc(100vh - 116.96px);
+    }
 `
 
 const NavLink = styled.div`
@@ -16,10 +22,60 @@ const NavLink = styled.div`
 const BackLink = styled(Link)`
     transition: 0.3s;
     &:hover{
-        text-shadow: 1px 1px 5px ${({theme}) => theme.color};
+        text-shadow: 1px 1px 5px ${({ theme }) => theme.color};
 
     }
 `
+
+const PokemonIdentitySection = styled.div`
+    padding: 2rem 0;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+const PokemonName = styled.h2`
+    text-transform: capitalize;
+    font-weight: bold;
+`
+
+const Id = styled.p`
+    font-weight: 600;
+`
+const Img = styled.img`
+    height: 200px;
+    width: auto;
+`
+const PokemonImgSection = styled.div`
+    max-width: 640px;
+    margin: 0 auto;
+    padding: 2rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    border-radius: 1rem;
+    background-color: ${({ type }) => pokemonTypeColors[type] || "gray"}; 
+
+`
+
+const TypesListSection = styled.ul`
+    padding: 2rem;
+    display: flex;
+    justify-content: center;
+    gap: 6rem;
+`
+
+const Types = styled.li`
+    padding: 0.2rem 1rem;
+    
+    text-transform: capitalize;
+    color: #fff;
+    background-color: ${({ type }) => pokemonTypeColors[type] || "gray"};
+    border-radius: 1rem;
+`
+
 
 const PokemonPage = () => {
     const { id } = useParams()
@@ -33,18 +89,18 @@ const PokemonPage = () => {
                 <BackLink to="/">Voltar</BackLink>
             </NavLink>
 
-            <div>
-                <h2>{pokemon.name}</h2>
-                <p>{pokemon.id}</p>
-            </div>
-
-            <img src={pokemon.sprites?.other.dream_world.front_default} />
-
-            <ul>
+            <PokemonIdentitySection>
+                <PokemonName>{pokemon.name}</PokemonName>
+                <Id>{pokemon.id}</Id>
+            </PokemonIdentitySection>
+            <PokemonImgSection type={pokemon.types?.[0].type.name}>
+                <Img src={pokemon.sprites?.other.dream_world.front_default} />
+            </PokemonImgSection>
+            <TypesListSection>
                 {pokemon.types?.map((type, index) => (
-                    <li key={index}>{type.type.name}</li>
+                    <Types type={type.type.name} key={index}>{type.type.name}</Types>
                 ))}
-            </ul>
+            </TypesListSection>
             <InfoTabScreen />
         </Container>
     )
